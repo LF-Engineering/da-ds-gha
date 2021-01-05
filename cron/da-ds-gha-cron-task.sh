dfc332b0679bd70a4bc8aaa250c5321381f053e0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-01-05 10:31:56 +0000

    Cron scripts to deploy on test EC2
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ cron/da-ds-gha-cron-task.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+date
+if [ -z "$1" ]
+then
+  echo "$0: you need to specify name for this run, can be 'da-ds-gha-test' for example"
+  exit 1
+fi
+if [ -z "$2" ]
+then
+  echo "$0: you need to specify environment as a 2nd arg: test|prod"
+  exit 2
+fi
+cd /root/go/src/github.com/LF-Engineering/da-ds-gha/ || exit 3
+git pull || exit 4
+repo="`cat repo_access.secret`"
+if [ -z "$repo" ]
+then
+  echo "$0: missing repo_access.secret file"
+  exit 5
+fi
+rm -rf dev-analytics-api
+git clone "${repo}" || exit 6
+cd dev-analytics-api || exit 7
+git checkout "$2" || exit 8
+lock_file="/tmp/$1.lock"
+function cleanup {
+  rm -f "${lock_file}" dev-analytics-api
+}
+if [ -f "${lock_file}" ]
+then
+  # echo "$0: another da-ds-gha instance \"$1\" is still running, exiting"
+  exit 9
+fi
+> "${lock_file}"
+trap cleanup EXIT
+./dadsgha ./dev-analytics-api/app/services/lf/bootstrap/fixtures 2>&1 | tee -a run.log
---
+++ cron/da-ds-gha-prod.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+export GHA_NCPUS=''
+export GHA_NO_GHA_MAP=''
+export GHA_DB_BULK_SIZE=''
+export GHA_ES_BULK_SIZE=''
+export GHA_NO_INCREMENTAL=''
+export GHA_DEBUG=''
+export GHA_LOAD_CONFIG=''
+export GHA_SAVE_CONFIG=''
+export GHA_HOUR_FROM=''
+export GHA_HOUR_TO=''
+export GHA_DAY_FROM=''
+export GHA_DAY_TO=''
+export GHA_ES_URL="`cat ES_URL.prod.secret`"
+export GHA_DB_CONN="`cat DB_CONN.prod.secret`"
+export GHA_GITHUB_OAUTH="`cat OAUTHS.secret`"
+if [ -z "$GHA_ES_URL" ]
+then
+  echo "$0: missing GHA_ES_URL env variable, exiting"
+  exit 1
+fi
+if [ -z "$GHA_DB_CONN" ]
+then
+  echo "$0: missing GHA_DB_CONN env variable, exiting"
+  exit 2
+fi
+if [ -z "$GHA_GITHUB_OAUTH" ]
+then
+  echo "$0: missing GHA_GITHUB_OAUTH env variable, exiting"
+  exit 1
+fi
+/usr/bin/da-ds-gha-cron-task.sh da-ds-gha-prod prod 1>> /tmp/da-ds-gha-prod.log 2>>/tmp/da-ds-gha-prod.err
